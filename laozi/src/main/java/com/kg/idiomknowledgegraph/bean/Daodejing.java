da80ad3ff615beac15794d08f783ca6b1a26bd7f
Bread-Hu <506058680@qq.com>
2021-05-16 15:37:25 +0800

    Initial commit

--- laozi/src/main/java/com/kg/idiomknowledgegraph/bean/Daodejing.java
+++ laozi/src/main/java/com/kg/idiomknowledgegraph/bean/Daodejing.java
@@ -1,57 +1,11 @@
 package com.kg.idiomknowledgegraph.bean;
 
+import lombok.Data;
+
+@Data
 public class Daodejing {
     private int chapter;
     private String original;
     private String translation;
     private String annotation;
-
-    public Daodejing(int chapter, String original, String translation, String annotation) {
-        this.chapter = chapter;
-        this.original = original;
-        this.translation = translation;
-        this.annotation = annotation;
-    }
-
-    public int getChapter() {
-        return chapter;
-    }
-
-    public void setChapter(int chapter) {
-        this.chapter = chapter;
-    }
-
-    public String getOriginal() {
-        return original;
-    }
-
-    public void setOriginal(String original) {
-        this.original = original;
-    }
-
-    public String getTranslation() {
-        return translation;
-    }
-
-    public void setTranslation(String translation) {
-        this.translation = translation;
-    }
-
-    public String getAnnotation() {
-        return annotation;
-    }
-
-    public void setAnnotation(String annotation) {
-        this.annotation = annotation;
-    }
-
-    @Override
-    public String toString() {
-        return "Daodejing{" +
-                "chapter=" + chapter +
-                ", original='" + original + '\'' +
-                ", translation='" + translation + '\'' +
-                ", annotation='" + annotation + '\'' +
-                '}';
-    }
 }
