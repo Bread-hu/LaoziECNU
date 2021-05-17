c2e3858afb4fd7b1a7b6ff8d362c4f4f6a46aa77
Bread-Hu <506058680@qq.com>
2021-05-17 15:45:56 +0800

    Initial commit

--- laozi/src/main/java/com/kg/idiomknowledgegraph/controller/LaoziController.java
+++ laozi/src/main/java/com/kg/idiomknowledgegraph/controller/LaoziController.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.InvocationTargetException;
+
 
 @Controller
 @RequestMapping("/laozi")
@@ -14,6 +16,8 @@ public class LaoziController {
 
     @Autowired
     private LaoziService laoziService;
+
+    private Daodejing daodejing;
 
     @GetMapping("/daodejing")
     public String toDaodejing(Model model) {
@@ -23,7 +27,14 @@ public class LaoziController {
     @PostMapping("/daodejing")
     @ResponseBody
     public Daodejing getContentByChapter(int chapter){
-        return laoziService.getDaodejing(chapter);
+        this.daodejing=laoziService.getDaodejing(chapter);
+        return this.daodejing;
+    }
+
+    @PostMapping("/selectLanguage")
+    @ResponseBody
+    public String selectLanguage(String language) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        return (String) this.daodejing.getClass().getMethod("get"+language,new Class[]{}).invoke(this.daodejing,new Object[]{});
     }
 
 }
