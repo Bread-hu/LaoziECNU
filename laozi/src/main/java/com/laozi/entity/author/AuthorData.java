f9f164996bf2a21d8b6bc2df380971d387062051
tatataeki <1490065055@qq.com>
2021-06-08 12:35:49 +0800

    add authorData

---
+++ laozi/src/main/java/com/laozi/entity/author/AuthorData.java
@@ -0,0 +1,22 @@
+package com.laozi.entity.author;
+
+public class AuthorData {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    String title;
+    String content;
+}
